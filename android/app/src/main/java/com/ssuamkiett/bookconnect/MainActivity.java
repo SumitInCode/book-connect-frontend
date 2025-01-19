410d2f60bfc70aeb925e27895cc08143359b2185
SumitInCode <sumitkumar7033@gmail.com>
2025-01-19 11:55:44 +0530

    fix: Workaround for status bar light color.

--- android/app/src/main/java/com/ssuamkiett/bookconnect/MainActivity.java
+++ android/app/src/main/java/com/ssuamkiett/bookconnect/MainActivity.java
@@ -2,4 +2,16 @@ package com.ssuamkiett.bookconnect;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+      @Override
+      @RequiresApi(api = Build.VERSION_CODES.M)
+      protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+
+          // Enforce light status bar
+          getWindow().getDecorView().setSystemUiVisibility(
+              View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+          );
+          getWindow().setStatusBarColor(android.graphics.Color.WHITE);
+      }
+}
